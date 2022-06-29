eef4492cee4e608ec461a98a0b5aff92322a83c6
vencesla <admirmabiki@gmail.com>
2022-06-29 10:37:21 +0200

    ajout des pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Home from "./pages/Home";
 import {BrowserRouter, Routes, Route} from  'react-router-dom'
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile.js";
 const  App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
       </Routes>
     </BrowserRouter>
   );
