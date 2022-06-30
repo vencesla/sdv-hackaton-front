0e76caef1b1fb4ed7290debe116ed70668432488
Martin Barré <martin.barre29@gmail.com>
2022-06-30 23:57:52 +0200

    Merge and fix conflict

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -63,115 +63,117 @@ const Profile = ({ user, refreshUser }) => {
 
     return (
         <div className="dashboard">
-            <MatchList user={user}/>
+            <section>
+                <MatchList user={user} refreshUser={refreshUser}/>
+            </section>
 
             <div className="profile-core">
-            <div className="profile-page">
-                <div className="phone-screen">
-                    <div className="profile-top">
-                        <Link className="profile-back" to="/dashboard"><ArrowLeft/></Link>
-                        <h2 className="pb-3">Modifier votre profil</h2>
+                <div className="profile-page">
+                    <div className="phone-screen">
+                        <div className="profile-top">
+                            <Link className="profile-back" to="/dashboard"><ArrowLeft/></Link>
+                            <h2 className="pb-3">Modifier votre profil</h2>
+                        </div>
+
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-floating mb-3">
+                                <input
+                                    id="firstName"
+                                    name="firstName"
+                                    className="form-control"
+                                    value={formData.firstName}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <label htmlFor="firstName">Prénom</label>
+                            </div>
+
+                            <div className="form-floating mb-3">
+                                <input
+                                    id="lastName"
+                                    name="lastName"
+                                    className="form-control"
+                                    value={formData.lastName}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <label htmlFor="lastName">Nom</label>
+                            </div>
+
+                            <div className="form-floating mb-3">
+                            <textarea
+                                id="description"
+                                name="description"
+                                className="form-control"
+                                value={formData.description}
+                                onChange={handleChange}
+                                rows="3"
+                            />
+                                <label htmlFor="description">Description</label>
+                            </div>
+
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="number"
+                                    id="age"
+                                    name="age"
+                                    className="form-control"
+                                    value={formData.age}
+                                    onChange={handleChange}
+                                    min="1"
+                                    max="150"
+                                    required
+                                />
+                                <label htmlFor="age">Age</label>
+                            </div>
+
+                            <div className="form-floating mb-3">
+                                <input
+                                    id="phoneNumber"
+                                    name="phoneNumber"
+                                    className="form-control"
+                                    value={formData.phoneNumber}
+                                    onChange={handleChange}
+                                />
+                                <label htmlFor="phoneNumber">Téléphone</label>
+                            </div>
+
+                            <div className="mb-3">
+                                <label>Je recherche</label>
+                            </div>
+
+                            <div className="btn-group mb-4" role="group">
+                                <input type="radio" className="btn-check" name="btnradio" id="btnradio1" required
+                                       checked={formData.gender === 'employee'} value="employee" onClick={() => formData.gender = 'employee'} onChange={handleChange}/>
+                                <label className="btn btn-outline-primary" htmlFor="btnradio1">Une entreprise</label>
+
+                                <input type="radio" className="btn-check" name="btnradio" id="btnradio2"
+                                       checked={formData.gender === 'employer'} value="employer" onClick={() => formData.gender = 'employer'} onChange={handleChange}/>
+                                <label className="btn btn-outline-primary" htmlFor="btnradio2">Un talent</label>
+                            </div>
+
+                            <div className="form-floating mb-3">
+                                <input
+                                    id="country"
+                                    name="country"
+                                    className="form-control"
+                                    value={formData.country}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <label htmlFor="country">Pays</label>
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="btn btn-success w-100"
+                            >
+                                Valider
+                            </button>
+                        </form>
                     </div>
-
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-floating mb-3">
-                            <input
-                                id="firstName"
-                                name="firstName"
-                                className="form-control"
-                                value={formData.firstName}
-                                onChange={handleChange}
-                                required
-                            />
-                            <label htmlFor="firstName">Prénom</label>
-                        </div>
-
-                        <div className="form-floating mb-3">
-                            <input
-                                id="lastName"
-                                name="lastName"
-                                className="form-control"
-                                value={formData.lastName}
-                                onChange={handleChange}
-                                required
-                            />
-                            <label htmlFor="lastName">Nom</label>
-                        </div>
-
-                        <div className="form-floating mb-3">
-                        <textarea
-                            id="description"
-                            name="description"
-                            className="form-control"
-                            value={formData.description}
-                            onChange={handleChange}
-                            rows="3"
-                        />
-                            <label htmlFor="description">Description</label>
-                        </div>
-
-                        <div className="form-floating mb-3">
-                            <input
-                                type="number"
-                                id="age"
-                                name="age"
-                                className="form-control"
-                                value={formData.age}
-                                onChange={handleChange}
-                                min="1"
-                                max="150"
-                                required
-                            />
-                            <label htmlFor="age">Age</label>
-                        </div>
-
-                        <div className="form-floating mb-3">
-                            <input
-                                id="phoneNumber"
-                                name="phoneNumber"
-                                className="form-control"
-                                value={formData.phoneNumber}
-                                onChange={handleChange}
-                            />
-                            <label htmlFor="phoneNumber">Téléphone</label>
-                        </div>
-
-                        <div className="mb-3">
-                            <label>Je recherche</label>
-                        </div>
-
-                        <div className="btn-group mb-4" role="group">
-                            <input type="radio" className="btn-check" name="btnradio" id="btnradio1" required
-                                   checked={formData.gender === 'employee'} value="employee" onClick={() => formData.gender = 'employee'} onChange={handleChange}/>
-                            <label className="btn btn-outline-primary" htmlFor="btnradio1">Une entreprise</label>
-
-                            <input type="radio" className="btn-check" name="btnradio" id="btnradio2"
-                                   checked={formData.gender === 'employer'} value="employer" onClick={() => formData.gender = 'employer'} onChange={handleChange}/>
-                            <label className="btn btn-outline-primary" htmlFor="btnradio2">Un talent</label>
-                        </div>
-
-                        <div className="form-floating mb-3">
-                            <input
-                                id="country"
-                                name="country"
-                                className="form-control"
-                                value={formData.country}
-                                onChange={handleChange}
-                                required
-                            />
-                            <label htmlFor="country">Pays</label>
-                        </div>
-
-                        <button
-                            type="submit"
-                            className="btn btn-success w-100"
-                        >
-                            Valider
-                        </button>
-                    </form>
                 </div>
-            </div>
-            <NavTab/>
+                <NavTab/>
             </div>
         </div>
     );
